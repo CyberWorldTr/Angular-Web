eddc4d62fd959c02b74600bdbd9fe4f3eb6ba974
CyberWorldTr <cyberworldtur@gmail.com>
2022-04-16 02:24:22 +0300

    first commit

---
+++ src/app/app.menu.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './app.menu.component.html',
+  
+})
+export class AppMenuComponent {
+  constructor(){ }
+
+  ngOnInit(): void {
+      
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AppMenuComponent } from './app.menu.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppMenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
